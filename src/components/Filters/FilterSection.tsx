5846afdb492eafdd452f607be148cdd77685aa48
Tarun Lal Rajak <tarunlal@geekyants.com>
2024-05-06 00:33:14 +0530

    feat/added other filters in FilterSection component

--- src/components/Filters/FilterSection.tsx
+++ src/components/Filters/FilterSection.tsx
@@ -1,25 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./FilterSection.module.css";
+import Select from "react-select";
 
 type SearchValueType = {
   searchValue: string;
   setSearchValue: React.Dispatch<React.SetStateAction<string>>;
+  onChangeExperience: (value: any) => void;
+  locations: any;
+  handleLocationChange: any;
+  roleOptions: any;
+  handleRoleChange: any;
 };
 
-const FilterSection = ({ searchValue, setSearchValue }: SearchValueType) => {
-  const inputHandler = (value:string) => {
+const FilterSection = ({
+  searchValue,
+  setSearchValue,
+  onChangeExperience,
+  locations,
+  handleLocationChange,
+  roleOptions,
+  handleRoleChange,
+}: SearchValueType) => {
+  const [selectedOption, setSelectedOption] = useState(null);
+  const inputHandler = (value: string) => {
     setSearchValue(value);
   };
 
+  const options = Array.from({ length: 10 }, (_, i) => ({
+    value: i + 1,
+    label: `${i + 1}`,
+  }));
+  const handleChange = (selectedOption) => {
+    setSelectedOption(selectedOption);
+    if (onChangeExperience) {
+      onChangeExperience(selectedOption);
+    }
+  };
   return (
-    <div>
+    <div className={styles.filterOptions}>
       <input
         value={searchValue}
         placeholder="Search Company Name"
         className={styles.filterSearch}
         type="text"
-        onChange={(e)=>inputHandler(e.target.value)}
+        onChange={(e) => inputHandler(e.target.value)}
       />
+      <Select
+        value={selectedOption}
+        onChange={handleChange}
+        options={options}
+        placeholder="Min Experience"
+      />
+      <Select
+        options={locations}
+        onChange={handleLocationChange}
+        placeholder="Select Location"
+        isMulti
+      />
+      <Select
+        options={roleOptions}
+        onChange={handleRoleChange}
+        placeholder="Select Roles"
+        isMulti
+      />
+      {/* <Select
+        options={roleOptions}
+        onChange={handleBasePay}
+        placeholder="Minimum Base Pay Salary"
+        isMulti
+      /> */}
     </div>
   );
 };
--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -5,6 +5,7 @@ import styles from "./Homepage.module.css";
 import { CardDataType } from "../JobCard/JobCard.types";
 import { debounce } from "../../utils/constants";
 import FilterSection from "../Filters/FilterSection";
+import { SelectType } from "./Homepage.types";
 
 const Homepage = () => {
   const limit = 10;
@@ -12,19 +13,46 @@ const Homepage = () => {
   const [offset, setOffset] = useState(0);
   const [isloading, setIsLoading] = useState(true);
   const [searchValue, setSearchValue] = useState("");
+  const [selectedExperience, setSelectedExperience] = useState(null);
+  const [locations, setLocations] = useState([]);
+  const [selectedRoles, setSelectedRoles] = useState<SelectType[]>([]);
+  const [selectedLocation, setSelectedLocation] = useState<SelectType[]>([]);
 
-
-  const filteredJobCards = jobCards?.filter(job => {
-    const companyName = job.companyName ? job.companyName.toLowerCase() : '';
-    const searchValueLower = searchValue.toLowerCase(); 
-    return companyName.includes(searchValueLower);
+  const filteredJobCards = jobCards.filter((job) => {
+    const companyName = job.companyName ? job.companyName.toLowerCase() : "";
+    const searchValueLower = searchValue.toLowerCase();
+    const experienceFilter =
+      !selectedExperience ||
+      (job?.minExp <= selectedExperience?.value &&
+        job?.maxExp >= selectedExperience?.value);
+    const roleFilter =
+      selectedRoles.length === 0 ||
+      selectedRoles.some((role) => role.value === job.jobRole);
+    const locationFilter =
+      selectedLocation.length === 0 ||
+      selectedLocation.some((location) => location.value === job.location);
+    return (
+      companyName.includes(searchValueLower) &&
+      experienceFilter &&
+      roleFilter &&
+      locationFilter
+    );
   });
+  
 
   const fetchData = async () => {
     try {
       const responseData = await fetchJobCards(limit, offset);
       const newCards = responseData?.jdList;
       setJobCards((prevJobCards) => [...prevJobCards, ...newCards]);
+      const uniqueLocations = [
+        ...new Set(newCards.map((card) => card.location)),
+      ];
+      const locationsOptions = uniqueLocations.map((location) => ({
+        value: location,
+        label: location,
+      }));
+      setLocations(locationsOptions);
     } catch (error) {
       console.error(error);
     }
@@ -53,11 +81,31 @@ const Homepage = () => {
     };
   }, [debouncedScroll]);
 
+  const handleExperienceChange = (selectedExperience) => {
+    setSelectedExperience(selectedExperience);
+  };
+  const handleLocationChange = (selectedLocation:SelectType[]) => {
+    setSelectedLocation(selectedLocation);
+  };
+
+  const handleRoleChange = (selectedRole:SelectType[]) => {
+    setSelectedRoles(selectedRole);
+  };
+
+  const roleOptions = [...new Set(jobCards.map((card) => card.jobRole))].map(
+    (role) => ({ value: role, label: role })
+  );
+
   return (
     <div>
       <FilterSection
+        onChangeExperience={handleExperienceChange}
         setSearchValue={setSearchValue}
         searchValue={searchValue}
+        locations={locations}
+        handleLocationChange={handleLocationChange}
+        roleOptions={roleOptions}
+        handleRoleChange={handleRoleChange}
       />
       <div className={styles.jobCardsContainer}>
         {filteredJobCards?.map((jobCard, index) => (
